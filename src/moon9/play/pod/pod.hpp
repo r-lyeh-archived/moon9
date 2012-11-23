369a2035b100fe47c9455641f80708a6f1a2df20
r-lyeh <rlyeh.nospam@gmail.com>
2012-11-23 16:51:49 +0100

    play/pod, more types added

--- src/moon9/play/pod/pod.hpp
+++ src/moon9/play/pod/pod.hpp
@@ -5,10 +5,10 @@
 
 namespace moon9
 {
-    template<typename T, int div = 0, int num = 1>
+    template<typename T, int num = 0, int div = 1>
     struct pod
     {
-        pod() : t(T(float(div)/num))
+        pod() : t(T(float(num)/div))
         {}
 
         pod( const T &_t ) : t(_t)
@@ -33,28 +33,38 @@ namespace moon9
         protected: T t;
     };
 
-    typedef moon9::pod<double,-1>  double1n;
-    typedef moon9::pod<double,0>   double0;
-    typedef moon9::pod<double,1,2> doubleh;
-    typedef moon9::pod<double,1>   double1;
-    typedef moon9::pod<double,2>   double2;
+    typedef moon9::pod<          int,  -1 > int_1;
+    typedef moon9::pod<          int,   0 > int0;
+    typedef moon9::pod<          int,   1 > int1;
+    typedef moon9::pod<          int,   2 > int2;
+    typedef moon9::pod<          int,   3 > int3;
 
-    typedef moon9::pod<float,-1>  float1n;
-    typedef moon9::pod<float,0>   float0;
-    typedef moon9::pod<float,1,2> floath;
-    typedef moon9::pod<float,1>   float1;
-    typedef moon9::pod<float,2>   float2;
+    typedef moon9::pod<         bool,   0 > bool0;
+    typedef moon9::pod<         bool,   1 > bool1;
 
-    typedef moon9::pod<bool,0> bool0;
-    typedef moon9::pod<bool,1> bool1;
+    typedef moon9::pod<        float,  -1 > float_1;
+    typedef moon9::pod<        float,   0 > float0;
+    typedef moon9::pod<        float, 1,2 > float1_2;
+    typedef moon9::pod<        float, 1,3 > float1_3;
+    typedef moon9::pod<        float, 1,4 > float1_4;
+    typedef moon9::pod<        float, 1,5 > float1_5;
+    typedef moon9::pod<        float,   1 > float1;
+    typedef moon9::pod<        float,   2 > float2;
+    typedef moon9::pod<        float,   2 > float3;
 
-    typedef moon9::pod<int,-1> int1n;
-    typedef moon9::pod<int, 0> int0;
-    typedef moon9::pod<int, 1> int1;
-    typedef moon9::pod<int, 2> int2;
+    typedef moon9::pod<       double,  -1 > double_1;
+    typedef moon9::pod<       double,   0 > double0;
+    typedef moon9::pod<       double, 1,2 > double1_2;
+    typedef moon9::pod<       double, 1,3 > double1_3;
+    typedef moon9::pod<       double, 1,4 > double1_4;
+    typedef moon9::pod<       double, 1,5 > double1_5;
+    typedef moon9::pod<       double,   1 > double1;
+    typedef moon9::pod<       double,   2 > double2;
+    typedef moon9::pod<       double,   3 > double3;
 
-    typedef moon9::pod<unsigned int, 0> uint0;
-    typedef moon9::pod<unsigned int, 1> uint1;
-    typedef moon9::pod<unsigned int, 2> uint2;
-    typedef moon9::pod<unsigned int,~0> uintN;
+    typedef moon9::pod< unsigned int,   0 > uint0;
+    typedef moon9::pod< unsigned int,   1 > uint1;
+    typedef moon9::pod< unsigned int,   2 > uint2;
+    typedef moon9::pod< unsigned int,   3 > uint3;
+    typedef moon9::pod< unsigned int,  ~0 > uintN;
 }
